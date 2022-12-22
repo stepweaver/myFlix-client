d41d0c0fd3eec55e12ad5dad997a04c1853bfb95
Stephen Weaver <sweaver1@ND.EDU>
2022-12-22 11:16:50 -0500

    Update main-view.jsx
    import useState, then map over and list each element in the array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
+import { useState } from 'react';
+
 export const MainView = () => {
-  return (
-    <div>
-      <div>Eloquent Javascript</div>
-      <div>Mastering JavaScript Functional Programming</div>
-      <div>JavaScript: The Good Parts</div>
-      <div>JavaScript: The Definitive Guide</div>
-      <div>The Road to React</div>
-    </div>
-  );
+  const [movies, setMovies] = useState([
+    { id: 1, title: 'Eloquent JavaScript' },
+    { id: 2, title: 'Mastering JavaScript Function Programming' },
+    { id: 3, title: 'JavaScript: The Good Parts'},
+    { id: 4, title: 'JavaScript: The Definitive Guide'},
+    { id: 5, title: 'The Road to React'}
+  ]);
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  } else {
+    return (
+      <div>
+        {movies.map((movie) => {
+          return <div key={movie.id}>{movie.title}</div>;
+        })}
+      </div>
+    );
+  }
 };
