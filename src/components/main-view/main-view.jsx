cece33677efb6dea21924670b1c1701b10c3f841
Stephen Weaver <sweaver1@ND.EDU>
2022-12-22 10:10:37 -0500

    create MainView component

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const MainView = () => {
+  return (
+    <div>
+      <div>Eloquent Javascript</div>
+      <div>Mastering JavaScript Functional Programming</div>
+      <div>JavaScript: The Good Parts</div>
+      <div>JavaScript: The Definitive Guide</div>
+      <div>The Road to React</div>
+    </div>
+  );
+};
