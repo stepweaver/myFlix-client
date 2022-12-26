ca926345079677020677ef762c15c4fa0d5064d9
Stephen Weaver <sweaver1@ND.EDU>
2022-12-26 13:23:24 -0500

    Update movie-view.jsx
    Organized MovieView display

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -4,14 +4,19 @@ export const MovieView = ({ movieData, onBackClick }) => {
       <div>
         <img src={movieData.image} />
       </div>
+      <br />
+      <div>{movieData.title}</div><br />
       <div>
-        <span>Title: </span>
-        <span>{movieData.title}</span>
-      </div>
+        <span>Year: </span>
+        <span>{movieData.year}, </span>
+        <span>Rated: </span>
+        <span>{movieData.rating}</span>
+      </div><br />
       <div>
-        <span>Director: </span>
-        <span>{movieData.director}</span>
-      </div>
+        <span>Genre: </span>
+        <span>{movieData.genre}</span>
+      </div><br />
+      <div>{movieData.description}</div><br />
       <button onClick={onBackClick}>Back</button>
     </div>
   );
