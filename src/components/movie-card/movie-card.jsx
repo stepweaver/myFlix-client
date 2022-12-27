3f359b63d6663eb403fd5398d50f1e69bdc4ba32
Stephen Weaver <sweaver1@ND.EDU>
2022-12-27 15:16:33 -0500

    Update movie-card.jsx
    Import and define props for the MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export const MovieCard = ({ movieData, onMovieClick }) => {
   return (
     <div
@@ -8,4 +10,19 @@ export const MovieCard = ({ movieData, onMovieClick }) => {
       {movieData.title}
     </div>
   );
+};
+
+MovieCard.propTypes = {
+  movieData: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    year: PropTypes.string.isRequired,
+    rating: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+    actors: PropTypes.array.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
