51e9865958bb659386f5189ac1fd22305f0d4912
Stephen Weaver <sweaver1@ND.EDU>
2023-01-03 16:31:42 -0500

    Update main-view.jsx
    Initialize the user and token objects from localStorage

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,6 +6,8 @@ import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+  const storedToken = localStorage.getItem('token');
   const [ movies, setMovies ] = useState([]);
   const [ selectedMovie, setSelectedMovie ] = useState(null);
   const [ user, setUser ] = useState(null);
