10a2039bd78314d01d5baf1cbd624e5a1c1948c2
Stephen Weaver <sweaver1@ND.EDU>
2023-01-03 15:49:09 -0500

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,8 +6,8 @@ import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [ movies, setMovies ] = useState([]);
+  const [ selectedMovie, setSelectedMovie ] = useState(null);
   const [ user, setUser ] = useState(null);
 
     useEffect(() => {
