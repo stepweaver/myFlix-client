e4515ebe666721752a9c2ed3af00b30957b62853
Stephen Weaver <sweaver1@ND.EDU>
2023-01-04 11:42:05 -0500

    Update main-view.jsx
    Import SignUpView and render

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem('user'));
@@ -41,12 +42,16 @@ export const MainView = () => {
 
   if (!user) {
     return (
-      <LoginView
-        onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }}
-      />
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
     );
   }
 
