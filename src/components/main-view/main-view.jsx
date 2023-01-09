5390d0bf8e437fab7713babbff542a40d3520bc3
Stephen Weaver <sweaver1@ND.EDU>
2023-01-09 15:17:03 -0500

    Revert "Update main-view.jsx"
    This reverts commit 61598df1d7d5598b62700b4354d67b59b9d31a42.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -18,7 +18,9 @@ export const MainView = () => {
     useEffect(() => {
       if (!token) return;
 
-      fetch('https://cthulhuflix.onrender.com/movies')
+      fetch('https://cthulhuflix.onrender.com/movies', {
+        headers: { Authorization: `Bearer ${token}` }
+      })
         .then((response) => response.json())
         .then((data) => {
           const moviesFromApi = data.map((movie) => {
