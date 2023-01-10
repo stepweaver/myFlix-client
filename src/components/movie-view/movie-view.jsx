86e10e6ac6be0ea68566a0d808b47e3cf2ddd2eb
Stephen Weaver <sweaver1@ND.EDU>
2023-01-10 07:54:25 -0500

    replace movieData with movie

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,24 +1,24 @@
 import { Button } from 'react-bootstrap';
 
-export const MovieView = ({ movieData, onBackClick }) => {
+export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img className='w-50' src={movieData.image} />
+        <img className='w-50' src={movie.image} />
       </div>
       <br />
-      <div>{movieData.title}</div><br />
+      <div>{movie.title}</div><br />
       <div>
         <span>Year: </span>
-        <span>{movieData.year}, </span>
+        <span>{movie.year}, </span>
         <span>Rated: </span>
-        <span>{movieData.rating}</span>
+        <span>{movie.rating}</span>
       </div><br />
       <div>
         <span>Genre: </span>
-        <span>{movieData.genre}</span>
+        <span>{movie.genre}</span>
       </div><br />
-      <div>{movieData.description}</div><br />
+      <div>{movie.description}</div><br />
       <Button
         variant='primary'
         onClick={onBackClick}
