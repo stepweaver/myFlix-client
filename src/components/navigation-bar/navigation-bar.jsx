f2b4746551406926f154cffc213655ed74fdfef5
Stephen Weaver <sweaver1@ND.EDU>
2023-01-11 22:11:45 -0500

    Update navigation-bar.jsx
    Create navigation-bar component

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,1 +1,45 @@
-import { Navbar, Container, Nav }
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import logo from '../../../images/cthulhuLogo.png';
+
+export const NavigationBar = ({ user, onLoggedOut }) => {
+  return (
+    <Navbar variant='dark' expand='lg'>
+      <Container>
+        <Navbar.Brand as={Link} to='/'>
+          <img src={logo} />
+          CthulhuFlix
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            {!user && (
+              <>
+                <Nav.Link as={Link} to='/login'>
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to='/signup'>
+                  Signup
+                </Nav.Link>
+              </>
+            )}
+            {user && (
+              <>
+                <Nav.Link as={Link} to='/'>
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to='/profile'>
+                  Profile
+                </Nav.Link>
+                <Nav.Link onClick={onLoggedOut}>
+                  Logout
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
