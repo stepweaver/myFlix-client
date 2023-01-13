05beed81bd6751506a09874e0ad44299bf9a29e3
Stephen Weaver <sweaver1@ND.EDU>
2023-01-12 22:22:11 -0500

    Update movie-view.jsx
    Add styling to movieView

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -11,19 +11,24 @@ export const MovieView = ({ movies }) => {
       <div>
         <img className='w-50 mb-2' src={movie.imageURL} />
       </div>
-      <div className='mb-2'>
-        <span>{movie.title} | </span>
+      <div className='mb-2 fs-2 fw-bold'>
+        <span>{movie.title}</span>
+      </div>
+      <div className='mb-3 fs-5'>
+        <span>{movie.description}</span>
+        <hr></hr>
+      </div>
+      <div className='mb-1'>
         <span>Rated: {movie.rating}</span>
       </div>
-      <div className='mb-2'>
+      <div className='mb-1'>
         <span>Genre: </span>
         <span>{movie.genre}</span>
       </div>
-      <div className='mb-2'>
+      <div className='mb-1'>
         <span>Release Year: </span>
         <span>{movie.year}</span>
       </div>
-      <div>{movie.description}</div>
       <Link to={'/'}>
         <Button className='mt-4 outline-light primary' size='sm'>Back</Button>
       </Link>
