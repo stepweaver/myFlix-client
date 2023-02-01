bb0f009cca2ce4adb70810b6c60bd66af6e3301f
Stephen Weaver <sweaver1@ND.EDU>
2023-02-01 14:40:03 -0500

    Update main-view.jsx
    Sort Imports

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react';
-import { Row, Col, Container, Button } from 'react-bootstrap';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
-import { LoginView } from '../login-view/login-view';
-import { SignupView } from '../signup-view/signup-view';
 import { NavigationBar } from '../navigation-bar/navigation-bar';
 import { ProfileView } from '../profile-view/profile-view';
+import { SignupView } from '../signup-view/signup-view';
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem('user'));
