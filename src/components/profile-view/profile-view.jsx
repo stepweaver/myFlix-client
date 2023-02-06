506199a585d8a60363c82767e689ef4b84b60556
Stephen Weaver <sweaver1@ND.EDU>
2023-02-06 15:20:55 -0500

    Update profile-view.jsx
    Create ProfileView

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,1 +1,21 @@
 import React from 'react';
+
+import { UpdateView } from './updateView';
+import { UserInfo } from './user-info';
+import { FavoriteMovies } from './favorite-movies';
+import { DeleteUser } from './delete-user';
+
+export const ProfileView = ({ movies }) => {
+  const storedToken = localStorage.getItem('token');
+  
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+
+  return (
+    <>
+      <UserInfo user={storedUser} />
+      <UpdateView storedToken={storedToken} storedUser={storedUser} />
+      <DeleteUser storedToekn={storedToken} storedUser={storedUser} />
+      <FavortieMovies movies={movies} storedUser={storedUser} />
+    </>
+  );
+};
