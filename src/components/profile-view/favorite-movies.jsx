2bcd638a1341dad5bc88a7be17db86db1053f152
Stephen Weaver <sweaver1@ND.EDU>
2023-02-07 14:59:48 -0500

    Create favorite-movies.jsx

---
+++ src/components/profile-view/favorite-movies.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+import { MovieCard } from '../movie-card/movie-card';
+
+export const FavoriteMovies = ({ movies, storedUser }) => {
+  const [ user, setUser ] = useState(storedUser ? storedUser : null);
+  let favoriteMoviesList = movies.filter((m) =>
+    user.FavoriteMovies.includes(m.id)
+  );
+
+  return (
+    <Row>
+      {favoriteMoviesList.length === 0 ? (
+        <Col>No favorites selected</Col>
+      ) : (
+        <>
+          <div className='text-start h2 mb-4'>Favorites</div>
+          {favoriteMoviesList.map((movie) => (
+            <Col className='mb-5' key={movie.id} xs={12} sm={6} md={4} lg={3}>
+              <MovieCard
+                movieData={movie}
+                user={user}
+                updateUserOnFav={(user) => {
+                  console.log('User updated:', user);
+                  setUser(user);
+                  localStorage.setItem('user', JSON.stringify(user));
+                }}
+              />
+            </Col>
+          ))}
+        </>
+      )}
+    </Row>
+  );
+};
