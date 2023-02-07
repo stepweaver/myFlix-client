8bd2fe9c09d461a160cc100764c67a94aa2ad764
Stephen Weaver <sweaver1@ND.EDU>
2023-02-07 15:12:26 -0500

    Create delete-user.jsx

---
+++ src/components/profile-view/delete-user.jsx
@@ -0,0 +1,41 @@
+import { Button, Col } from 'react-bootstrap';
+
+export const DeleteUser = ({ storedToken, storedUser }) => {
+  const handleDeregister = () => {
+    const userWarning = confirm(
+      `Confirm delete user profile?`
+    );
+
+    userWarning === false ? alert('User profile NOT deleted')
+    : fetch(`https://cthulhuflix.onrender.com/users/${storedUser.username}`, {
+      method: 'DELETE',
+      headers: { Authorization: `Bearer ${storedToken}`,
+      'Content-Type': 'application/json'
+      }
+    })
+    .then((response) => {
+      if (response.ok) {
+        alert('User profile DELETED');
+        localStorage.clear();
+        window.location.reload();
+      } else {
+        alert('Something went wrong');
+      }
+    })
+    .catch((e) => console.log(e));
+  };
+
+  return (
+    <Col md={5} className='text-end px-4'>
+      <div>
+        <Button
+          onClick={() => handleDeregister(storedUser._id)}
+          className='button-delete'
+          variant='danger'
+        >
+          Delete Profile
+        </Button>
+      </div>
+    </Col>
+  );
+};
