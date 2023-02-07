6b1b33d9195c56f511ae7ccca8dd3327e1d41f3c
Stephen Weaver <sweaver1@ND.EDU>
2023-02-07 16:49:45 -0500

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -106,30 +106,3 @@ MovieView.propTypes = {
     rating: PropTypes.string.isRequired
   })).isRequired
 };
-
-    // <div>
-    //   <div>
-    //     <img className='w-50 mb-2' src={movie.imageURL} />
-    //   </div>
-    //   <div className='mb-2 fs-2 fw-bold'>
-    //     <span>{movie.title}</span>
-    //   </div>
-    //   <div className='mb-3 fs-5'>
-    //     <span>{movie.description}</span>
-    //     <hr></hr>
-    //   </div>
-    //   <div className='mb-1'>
-    //     <span>Rated: {movie.rating}</span>
-    //   </div>
-    //   <div className='mb-1'>
-    //     <span>Genre: </span>
-    //     <span>{movie.genre}</span>
-    //   </div>
-    //   <div className='mb-1'>
-    //     <span>Release Year: </span>
-    //     <span>{movie.year}</span>
-    //   </div>
-    //   <Link to={'/'}>
-    //     <Button className='mt-4 outline-light primary' size='sm'>Back</Button>
-    //   </Link>
-    // </div>
