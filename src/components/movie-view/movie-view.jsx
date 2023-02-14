a6acc30980461988c172af78ed639a6dce92ed7d
Stephen Weaver <sweaver1@ND.EDU>
2023-02-14 16:14:07 -0500

    Update movie-view.jsx
    Adjust spacing.
    
    Check movies length and only display similar movies if there are genre type matches.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -32,17 +32,16 @@ export const MovieView = ({ movies, user, updateUserOnFav }) => {
         <Col md={5} className='d-flex flex-column'>
           <Row className='d-flex flex-row justify-content-between'>
             <Col md={9} className='d-flex flex-column'>
-              <h1 className='my-0'>
+              <h2 className='my-0'>
                 <span>{movie.title}</span>
-              </h1>
-              <h5 className='mt-1 text-left text-muted'>
-                <span>{movie.year}</span>
-              </h5>
-              <span className='fw-bolder fs-4'>{movie.genre.name}</span>              
+              </h2>
+              <h5 className='mt-1 text-left text-muted fw-bolder'>
+                <span>{movie.year} | {movie.genre.name}</span>
+              </h5>             
               {/* <span className='mt-4 fs-5'>{movie.actors.join(', ')}</span> */}
             </Col>
           </Row>
-          <div className='mt-md-5 mb-4'>
+          <div className='mt-md-2 mb-4'>
             <span>{movie.description}</span>
           </div>
           <Row className='d-flex flex-row justify-content-between mt-auto mb-md-4'>
@@ -63,24 +62,25 @@ export const MovieView = ({ movies, user, updateUserOnFav }) => {
           </Row>
         </Col>
       </Row>
-      <Row>
-        <h2 className='mt-0 p-3'>Similar movies</h2>
-        <hr />
-        {console.log(similarMovies)}
-        {similarMovies.map((movie) => (
-          <Col className='mb-5' key={movie.id} xs={12} sm={6} md={4} lg={3}>
-            <MovieCard
-              movieData={movie}
-              user={user}
-              updateUserOnFav={(user) => {
-                console.log('Update user:', user);
-                setUser(user);
-                localStorage.setItem('user', JSON.stringify(user));
-              }}
-            />
-          </Col>
-        ))}
-      </Row>
+      {similarMovies.length > 0 && (
+        <Row>
+          <h2 className='mt-0 p-3'>Similar movies</h2>
+          <hr />
+          {similarMovies.map((movie) => (
+            <Col className='mb-5' key={movie.id} xs={12} sm={6} md={4} lg={3}>
+              <MovieCard
+                movieData={movie}
+                user={user}
+                updateUserOnFav={(user) => {
+                  console.log('Update user:', user);
+                  setUser(user);
+                  localStorage.setItem('user', JSON.stringify(user));
+                }}
+              />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
