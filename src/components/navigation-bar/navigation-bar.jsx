c208061bd46db6da28a063d27a9c1d51eaf5376b
Stephen Weaver <sweaver1@ND.EDU>
2023-02-22 14:46:37 -0500

    Update navigation-bar.jsx
    Reorganize navbar spacing and add Title header.

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,7 +1,8 @@
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import logo from '../../../images/cthulhuLogo.png';
+import './navigation-bar.scss';
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
   return (
@@ -9,40 +10,49 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
       collapseOnSelect
       variant='dark'
       expand='lg'
+      className='navbar-custom'
     >
       <Container>
-        <Navbar.Brand as={Link} to='/'>
-          <img src={logo} width='175' className='p-4'/>
-          CthulhuFlix
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='me-auto'>
-            {!user && (
-              <>
-                <Nav.Link as={Link} to='/login'>
-                  Login
-                </Nav.Link>
-                <Nav.Link as={Link} to='/register'>
-                  Signup
-                </Nav.Link>
-              </>
-            )}
-            {user && (
-              <>
-                <Nav.Link as={Link} to='/'>
-                  Home
-                </Nav.Link>
-                <Nav.Link as={Link} to={`/users/${user.username}`}>
-                  Profile
-                </Nav.Link>
-                <Nav.Link onClick={onLoggedOut}>
-                  Logout
-                </Nav.Link>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
+        <Row className='navbar-row'>
+          <Col>
+            <div className="d-flex justify-content-between w-100">
+              <Navbar.Brand as={Link} to='/' className='brand'>
+                <img src={logo} width='175' className='p-4'/>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            </div>
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='ms-auto text-end nav-links'>
+                {!user && (
+                  <>
+                    <Nav.Link as={Link} to='/login'>
+                      Login
+                    </Nav.Link>
+                    <Nav.Link as={Link} to='/register'>
+                      Signup
+                    </Nav.Link>
+                  </>
+                )}
+                {user && (
+                  <>
+                    <Nav.Link as={Link} to={'/'} className='fs-4'>
+                      Home
+                    </Nav.Link>
+                    <Nav.Link as={Link} to={`/users/${user.username}`} className='fs-4'>
+                      Profile
+                    </Nav.Link>
+                    <Nav.Link onClick={onLoggedOut} className='fs-4'>
+                      Logout
+                    </Nav.Link>
+                  </>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Col>
+          <Col>
+            <h1 className='text-center cthulhuflix'>CthulhuFlix</h1>        
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
