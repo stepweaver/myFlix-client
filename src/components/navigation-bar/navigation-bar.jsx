1d7960d5b9e84154a72fe36ce81795c995c6a365
Stephen Weaver <sweaver1@ND.EDU>
2023-02-23 12:41:38 -0500

    Update navigation-bar.jsx
    Remove Row and Col - separate by div to get items spaced evenly across page.

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -13,46 +13,44 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
       className='navbar-custom'
     >
       <Container>
-        <Row className='navbar-row'>
-          <Col>
-            <div className="d-flex justify-content-between w-100">
-              <Navbar.Brand as={Link} to='/' className='brand'>
-                <img src={logo} width='175' className='p-4'/>
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            </div>
-            <Navbar.Collapse id='basic-navbar-nav'>
-              <Nav className='me-auto fs-5 nav-links'>
-                {!user && (
-                  <>
-                    <Nav.Link as={Link} to='/register'>
-                      Register
-                    </Nav.Link>
-                    <Nav.Link className='mx-auto' as={Link} to='/'>
-                      Login
-                    </Nav.Link>
-                  </>
-                )}
-                {user && (
-                  <>
-                    <Nav.Link as={Link} to={'/'} className='fs-4'>
-                      Home
-                    </Nav.Link>
-                    <Nav.Link as={Link} to={`/users/${user.username}`} className='fs-4'>
-                      Profile
-                    </Nav.Link>
-                    <Nav.Link onClick={onLoggedOut} className='fs-4'>
-                      Logout
-                    </Nav.Link>
-                  </>
-                )}
-              </Nav>
-            </Navbar.Collapse>
-          </Col>
-          <Col>
-            <h1 className='text-center cthulhuflix'>CthulhuFlix</h1>       
-          </Col>
-        </Row>
+        <div className='col-sm'>
+          <Navbar.Brand as={Link} to='/' className='brand'>
+            <img src={logo} width='175' className='p-4'/>
+          </Navbar.Brand>
+        </div>
+        <div className='col-sm'>
+          <h1 className='text-center cthulhuflix'>CthulhuFlix</h1>       
+        </div>
+        <div className='col-sm'>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse className='text-center w-100 mb-3' id='basic-navbar-nav'>
+            <Nav className='fs-5 nav-links'>
+              {!user && (
+                <>
+                  <Nav.Link as={Link} to='/register'>
+                    Register
+                  </Nav.Link>
+                  <Nav.Link as={Link} to='/'>
+                    Login
+                  </Nav.Link>
+                </>
+              )}
+              {user && (
+                <>
+                  <Nav.Link as={Link} to={'/'} className='fs-3'>
+                    Home
+                  </Nav.Link>
+                  <Nav.Link as={Link} to={`/users/${user.username}`} className='fs-3'>
+                    Profile
+                  </Nav.Link>
+                  <Nav.Link onClick={onLoggedOut} className='fs-3'>
+                    Logout
+                  </Nav.Link>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
