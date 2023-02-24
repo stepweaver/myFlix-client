6118b30bba85fe6b93a7bfa939e8c2b136fab5a4
Stephen Weaver <sweaver1@ND.EDU>
2023-02-23 22:56:20 -0500

    Update update-view.jsx
    Add classes for styling plus more bootstrap styling.

--- src/components/profile-view/update-view.jsx
+++ src/components/profile-view/update-view.jsx
@@ -63,12 +63,12 @@ export const UpdateView = ({ storedToken, storedUser }) => {
       <Row>
         <Col>
           <CardGroup>
-            <Card className='bg-transparent p-3 border-primary'>
+            <Card className='bg-transparent p-3 border-primary update-card text-uppercase'>
                 <Card.Title className='fs-3 fw-bold'>Update User Info</Card.Title>
                 <Card.Text>* Indicates a required field</Card.Text>
                 <Form onSubmit={handleSubmit}>
                   <Form.Group controlId='forUsername' className='input-group-sm mt-2'>
-                    <Form.Label>New Username *</Form.Label>
+                    <Form.Label className='text-light'>New Username *</Form.Label>
                     <Form.Control
                       type='text'
                       onChange={(e) => setUsername(e.target.value)}
@@ -76,32 +76,38 @@ export const UpdateView = ({ storedToken, storedUser }) => {
                       minLength='5'
                       title='Minimum length: 5 characters'
                       placeholder='Enter new username'
+                      className='form-control'
+                      style={{ border: 'none', borderBottom: '1px solid green', borderRadius: 0, boxShadow: 'none' }}
                     />
                   </Form.Group>
                   <Form.Group controlId='forPassword' className='input-group-sm mt-2'>
-                    <Form.Label>New Password *</Form.Label>
+                    <Form.Label className='text-light'>New Password *</Form.Label>
                     <Form.Control
                       type='password'
                       onChange={(e) => setPassword(e.target.value)}
                       minLength='6'
                       title='Minimum length: 6 characters'
                       placeholder='Enter new password'
+                      className='form-control'
+                      style={{ border: 'none', borderBottom: '1px solid green', borderRadius: 0, boxShadow: 'none' }}
                     />
                   </Form.Group>
                   <Form.Group controlId='forEmail' className='input-group-sm mt-2'>
-                    <Form.Label>New Email</Form.Label>
+                    <Form.Label className='text-light'>New Email</Form.Label>
                     <Form.Control
                       type='email'
                       onChange={(e) => setEmail(e.target.value)}
                       placeholder='Enter new email'
+                      className='form-control'
+                      style={{ border: 'none', borderBottom: '1px solid green', borderRadius: 0, boxShadow: 'none' }}
                     />
-                    <Form.Text className='text-muted'>
+                    <Form.Text className='text-muted text-lowercase'>
                       We'll never share your email.
                     </Form.Text>
                   </Form.Group>
                   <Row>
                     <Col className='text-end'>
-                      <Button variant='primary' type='submit' className='text-light mt-3'>
+                      <Button variant='primary' type='submit' className='text-uppercase fs-5 mt-3 btn-primary w-100'>
                         Update
                       </Button>
                     </Col>
