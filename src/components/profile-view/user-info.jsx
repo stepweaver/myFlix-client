05ee131d90fbeb1bf168bad7dd301d09f54cef82
Stephen Weaver <sweaver1@ND.EDU>
2023-02-23 22:29:48 -0500

    Update user-info.jsx
    Implement card for UserInfo

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Card, CardGroup } from 'react-bootstrap';
 import moment from 'moment';
 
 export const UserInfo = ({ user }) => {
   let userBirthday = moment.utc(user.birth).format('MM/DD/YYYY');
 
   return (
-    <Row className='bg-transparent d-flex flex-lg-row ms-2 text-lg-center mt-lg-2 mt-2 mb-3'>
-      <Col>
-        <span className='text-muted'>Username: </span>
-        <span>{user.username}</span>
+    <Row>
+      <Col className='text-start mb-5'>
+        <CardGroup>
+          <Card className='bg-transparent mt-5 mb-5'>
+            <Card.Title className='mb-4 text-uppercase fs-4 fw-bold'>User Info</Card.Title>
+            <Card.Text className='fs-5'>Username: {user.username}</Card.Text>
+            <Card.Text className='fs-5'>Email: {user.email}</Card.Text>
+            <Card.Text className='fs-5'>Birthday: {userBirthday}</Card.Text>
+          </Card>
+        </CardGroup>
       </Col>
-      <Col>
-        <span className='text-muted'>Email: </span>
-        <span>{user.email}</span>
-      </Col>
-      <Col>
-        <span className='text-muted'>Birthday: </span>
-        <span>{userBirthday}</span>
-      </Col>
-      <hr className='mt-3' />
     </Row>
   );
 };
