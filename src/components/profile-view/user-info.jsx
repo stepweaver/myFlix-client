9466d8ee3ae5ba608a401caf510714d4ce04ebcc
Stephen Weaver <sweaver1@ND.EDU>
2023-03-18 13:40:17 -0400

    Update user-info.jsx
    margin adjust

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -9,11 +9,10 @@ export const UserInfo = ({ user }) => {
     <Row>
       <Col className='text-start mb-5'>
         <CardGroup>
-          <Card className='bg-transparent mt-5 mb-5'>
-            <Card.Title className='mb-4 text-uppercase fs-4'>User Info</Card.Title>
-            <Card.Text className='fs-5'>Username: {user.username}</Card.Text>
-            <Card.Text className='fs-5'>Email: {user.email}</Card.Text>
-            <Card.Text className='fs-5'>Birthday: {userBirthday}</Card.Text>
+          <Card className='bg-transparent mt-3 mb-5'>
+            <Card.Title className='mb-4 text-uppercase fs-3'>User Info</Card.Title>
+            <Card.Text className='fs-5 mt-5'>Username: {user.username}</Card.Text>
+            <Card.Text className='fs-5 mt-5'>Email: {user.email}</Card.Text>
           </Card>
         </CardGroup>
       </Col>
