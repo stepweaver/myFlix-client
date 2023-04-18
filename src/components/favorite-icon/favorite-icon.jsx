e8d77218cd498206a3e7975cd4111d108cd8888c
Stephen Weaver <sweaver1@ND.EDU>
2023-04-18 16:37:35 -0400

    Update favorite-icon.jsx
    Toggle favorite icon plus to minus

--- src/components/favorite-icon/favorite-icon.jsx
+++ src/components/favorite-icon/favorite-icon.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { BsPlusSquare } from 'react-icons/bs';
+import { FiPlusSquare, FiMinusSquare } from 'react-icons/all';
 
 export const FavoriteIcon = ({ user, movie, updateUserOnFav }) => {
   let updatedUser = { ...user };
@@ -10,9 +10,11 @@ export const FavoriteIcon = ({ user, movie, updateUserOnFav }) => {
 
   const token = localStorage.getItem('token');
 
-  const alreadyFavorite = updatedUser.FavoriteMovies && updatedUser.FavoriteMovies.find(
-    (favMovieId) => favMovieId === movie.id
-  );
+  const alreadyFavorite =
+    updatedUser.FavoriteMovies &&
+    updatedUser.FavoriteMovies.find((favMovieId) => favMovieId === movie.id);
+
+  const [isFavorite, setIsFavorite] = useState(alreadyFavorite);
 
   const toggleFavorite = () => {
     if (!token) return;
@@ -23,42 +25,39 @@ export const FavoriteIcon = ({ user, movie, updateUserOnFav }) => {
       method: '',
       headers: {
         Authorization: `Bearer ${token}`,
-      }
+      },
     };
 
     let resultAlert = '';
-    let iconChange;
 
     if (alreadyFavorite) {
       requestOptions.method = 'DELETE';
       resultAlert = `${movie.title} removed from Favorites`;
-      iconChange = () =>
-        document.querySelector('svg').classList.remove('favorite-movie');
-      updatedUser.FavoriteMovies = updatedUser.FavoriteMovies.filter((favMovieId) => favMovieId !== movie.id);
+      updatedUser.FavoriteMovies = updatedUser.FavoriteMovies.filter(
+        (favMovieId) => favMovieId !== movie.id
+      );
     } else {
       requestOptions.method = 'POST';
       resultAlert = `${movie.title} added to Favorites`;
-      iconChange = () =>
-      document.querySelector('svg').classList.add('favorite-movie');
       updatedUser.FavoriteMovies.push(movie.id);
     }
 
     fetch(url, requestOptions)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      alert(`${resultAlert}`);
-      updateUserOnFav(updatedUser);
-      iconChange();
-    })
-    .catch((e) => {
-      alert('Oops! Something went wrong.');
-    });
-};
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        alert(`${resultAlert}`);
+        updateUserOnFav(updatedUser);
+        setIsFavorite(!isFavorite);
+      })
+      .catch((e) => {
+        alert('Oops! Something went wrong.');
+      });
+  };
 
   return (
     <Link
@@ -67,8 +66,14 @@ export const FavoriteIcon = ({ user, movie, updateUserOnFav }) => {
       id='favMovieButton'
       style={{ textDecoration: 'none', color: 'green' }}
     >
-      {alreadyFavorite ? <BsPlusSquare className='favorite-movie' style={{ fontSize: '30px' }} /> : <BsPlusSquare style={{ fontSize: '35px' }} />}
-      <span className='fw-bold' style={{ marginLeft: '10px', fontSize: '20px' }}>{alreadyFavorite ? 'Remove from Favorites' : 'Add to Favorites'}</span>
+      {isFavorite ? (
+        <FiMinusSquare className='favorite-movie' style={{ fontSize: '35px' }} />
+      ) : (
+        <FiPlusSquare style={{ fontSize: '35px' }} />  
+      )}
+      <span className='fw-bold' style={{ marginLeft: '10px', fontSize: '20px' }}>
+        {isFavorite ? 'Remove Favorite' : 'Add Favorites'}
+      </span>
     </Link>
   );
 };
