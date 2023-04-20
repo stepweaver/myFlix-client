0498a9580dcf3544fad0c41605e8e8ebdef078d0
Stephen Weaver <sweaver1@ND.EDU>
2023-04-20 09:53:13 -0400

    Add Search Box and improve layout

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,4 +1,4 @@
-import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import { Navbar, Container, Nav, Form, Button} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import logo from '../../../images/cthulhuLogo.png';
@@ -10,47 +10,44 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
       collapseOnSelect
       variant='dark'
       expand='lg'
-      className='navbar-custom'
     >
-      <Container>
-        <div className='col-sm'>
-          <Navbar.Brand as={Link} to='/' className='brand'>
-            <img src={logo} width='120' className='mt-4' />
-          </Navbar.Brand>
-        </div>
-        <div className='col-sm'>
-          <h1 className='text-center cthulhuflix'>CthulhuFlix</h1>       
-        </div>
-        <div className='col-sm me-auto'>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='fs-5 nav-links'>
-              {!user && (
-                <>
-                  <Nav.Link className='me-auto' as={Link} to='/register'>
-                    Register
-                  </Nav.Link>
-                  <Nav.Link className='me-auto' as={Link} to='/'>
-                    Login
-                  </Nav.Link>
-                </>
-              )}
-              {user && (
-                <>
-                  <Nav.Link as={Link} to={'/'}>
-                    Home
-                  </Nav.Link>
-                  <Nav.Link as={Link} to={`/users/${user.username}`}>
-                    Profile
-                  </Nav.Link>
-                  <Nav.Link onClick={onLoggedOut}>
-                    Logout
-                  </Nav.Link>
-                </>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </div>
+      <Container fluid>
+        <Navbar.Brand as={Link} to='/' className='brand'>
+          <img src={logo} alt='brand logo' width='70' />
+        </Navbar.Brand>
+        <h1 className='text-center cthulhuflix mt-4'>CthulhuFlix</h1>       
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto p-4 mt-4 nav-links'>
+            {!user && (
+              <>
+                <Nav.Link className='me-auto' as={Link} to='/register'>
+                  Register
+                </Nav.Link>
+                <Nav.Link className='me-auto' as={Link} to='/'>
+                  Login
+                </Nav.Link>
+              </>
+            )}
+            {user && (
+              <>
+                <Nav.Link as={Link} to={'/'}>
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to={`/users/${user.username}`}>
+                  Profile
+                </Nav.Link>
+                <Nav.Link onClick={onLoggedOut}>
+                  Logout
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+          <Form className='d-flex'>
+            <Form.Control type='Search' placeholder='Search Movies...' className='search-box me-2' aria-label='Search' />
+            <Button variant='outline-success' className='search-button'>Search</Button>
+          </Form>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
