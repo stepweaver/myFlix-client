2f0cb7b4b93b9f83cb0d6827950775de05e84151
Stephen Weaver <sweaver1@ND.EDU>
2023-04-21 13:12:29 -0400

    Update login-view.jsx
    Remove unnecessary bootstrap and narrow Login card

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, Form, Card, CardGroup, Container, Row, Col } from 'react-bootstrap';
+import { Button, Form, Card } from 'react-bootstrap';
 
 import './login-view.scss';
 
@@ -17,10 +17,13 @@ export const LoginView = ({ onLoggedIn }) => {
 
     fetch('https://cthulhuflix.onrender.com/login?' + new URLSearchParams(data).toString(), {
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify(data)
     })
-    .then ((response) => response.json())
-    .then ((data) => {
+    .then((response) => response.json())
+    .then((data) => {
       console.log('Login Response: ', data);
       if (data.user) {
         localStorage.setItem('user', JSON.stringify(data.user));
@@ -36,47 +39,41 @@ export const LoginView = ({ onLoggedIn }) => {
   };
 
   return (
-    <Container>
-      <Row>
-        <Col>
-          <CardGroup>
-            <Card className='login-card bg-transparent p-3 mt-5 border-primary text-uppercase'>
-              <Card.Title className='text-center fs-4 card-title'>Login</Card.Title>
-              <Form onSubmit={handleSubmit}>
-                <Form.Group controlId='loginUsername' className='input-group-sm mt-2'>
-                <Form.Label>Username</Form.Label>
-                  <Form.Control
-                    type='text' 
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    placeholder='Enter Username'
-                    minLength='2'
-                    required
-                    style={{ border: 'none', borderBottom: '1px solid green', borderRadius: 0, boxShadow: 'none' }}
-                  />
-                </Form.Group>
-                <Form.Group controlId='loginPassword' className='input-group-sm mt-2'>
-                <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type='password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder='Enter Password'
-                    required
-                    style={{ border: 'none', borderBottom: '1px solid green', borderRadius: 0, boxShadow: 'none' }}
-                  />
-                </Form.Group>
-                <Button
-                  className='mt-3 text-uppercase w-100 fw-bold fs-5'
-                  type='submit'
-                >
-                  Submit
-                </Button>
-              </Form>
-            </Card>
-          </CardGroup>
-        </Col>
-      </Row>
-    </Container>
+    <Card className='login-card bg-transparent p-3 mt-5' style={{ width: '20rem' }}>
+      <Card.Body>
+        <Card.Title className='text-center fs-4 card-title text-uppercase'>Login</Card.Title>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId='loginUsername' className='input-group-sm mt-2'>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type='text' 
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder='Enter Username'
+              minLength='2'
+              required
+              style={{ border: 'none', borderBottom: '1px solid green', borderRadius: 0, boxShadow: 'none' }}
+            />
+          </Form.Group>
+          <Form.Group controlId='loginPassword' className='input-group-sm mt-2'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Enter Password'
+              required
+              style={{ border: 'none', borderBottom: '1px solid green', borderRadius: 0, boxShadow: 'none' }}
+            />
+          </Form.Group>
+          <Button
+            className='mt-3 text-uppercase w-100 fw-bold fs-5'
+            type='submit'
+          >
+            Submit
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
   );
 };
