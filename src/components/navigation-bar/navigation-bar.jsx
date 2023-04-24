06dd79eda70bcc490ddbc616d2c1f8816c9d2544
Stephen Weaver <sweaver1@ND.EDU>
2023-04-24 11:08:21 -0400

    Update navigation-bar.jsx
    only render search box if user is logged in

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,4 +1,4 @@
-import { Navbar, Container, Nav, Form, Button} from 'react-bootstrap';
+import { Navbar, Container, Nav, Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
@@ -6,55 +6,59 @@ import logo from '../../../images/cthulhuLogo.png';
 import './navigation-bar.scss';
 
 export const NavigationBar = ({ user, onLoggedOut, onSearch }) => {
-  const [ query, setQuery ] = useState('');
+  const [query, setQuery] = useState('');
 
-  // useEffect(() => {
-  //   onSearch(query);
-  // }, [query]);
+  useEffect(() => {
+    onSearch(query);
+  }, [query]);
 
   return (
-    <Navbar
-      collapseOnSelect
-      variant='dark'
-      expand='lg'
-    >
+    <Navbar collapseOnSelect variant="dark" expand="lg">
       <Container fluid>
-        <Navbar.Brand as={Link} to='/' onClick={() => setQuery('')}>
-          <h1 className='text-center cthulhuflix mt-4'>
-            <img src={logo} alt='brand logo' width='70' />  CthulhuFlix
+        <Navbar.Brand as={Link} to="/" onClick={() => setQuery('')}>
+          <h1 className="text-center cthulhuflix mt-4">
+            <img src={logo} alt="brand logo" width="70" /> CthulhuFlix
           </h1>
-        </Navbar.Brand>       
-        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-        <Navbar.Collapse id='responsive-navbar-nav'>
-          <Nav className='me-auto p-3 mt-3 nav-links'>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto p-3 mt-3 nav-links">
             {!user && (
               <>
-                <Nav.Link className='me-auto' as={Link} to='/register'>
+                <Nav.Link className="me-auto" as={Link} to="/register">
                   Register
                 </Nav.Link>
-                <Nav.Link className='me-auto' as={Link} to='/'>
+                <Nav.Link className="me-auto" as={Link} to="/">
                   Login
                 </Nav.Link>
               </>
             )}
             {user && (
               <>
-                <Nav.Link as={Link} to={'/'}>
+                <Nav.Link as={Link} to={"/"}>
                   Home
                 </Nav.Link>
                 <Nav.Link as={Link} to={`/users/${user.username}`}>
                   Profile
                 </Nav.Link>
-                <Nav.Link onClick={onLoggedOut}>
-                  Logout
-                </Nav.Link>
+                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
               </>
             )}
           </Nav>
-          <Form className='d-flex'>
-            <Form.Control type='Search' placeholder='Search Movies...' className='search-box me-2' aria-label='Search' />
-            <Button variant='outline-success' className='search-button'>Search</Button>
-          </Form>
+          {user && (
+            <Form className="d-flex">
+              <Form.Control
+                type="Search"
+                placeholder="Search Movies..."
+                className="search-box me-2"
+                aria-label="Search"
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <Button variant="outline-success" className="search-button">
+                Search
+              </Button>
+            </Form>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
