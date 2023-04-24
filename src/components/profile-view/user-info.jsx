5a1543d70abf403597dd69cbb38fcf4e1d28d589
Stephen Weaver <sweaver1@ND.EDU>
2023-04-24 13:24:40 -0400

    Adjust spacing

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import { Row, Col, Card, CardGroup } from 'react-bootstrap';
-import moment from 'moment';
+
+import './user-info.scss';
 
 export const UserInfo = ({ user }) => {
 
   return (
-    <Row>
-      <Col className='text-start mb-5'>
-        <CardGroup>
-          <Card className='bg-transparent mt-3 mb-5'>
-            <Card.Title className='mb-4 text-uppercase fs-3'>User Info</Card.Title>
-            <Card.Text className='fs-5 mt-5'>USERNAME: {user.username}</Card.Text>
-            <Card.Text className='fs-5 mt-5'>EMAIL: {user.email}</Card.Text>
-          </Card>
-        </CardGroup>
-      </Col>
-    </Row>
+    <Col className='text-start mt-3'>
+      <h2 className='user-info text-uppercase fs-3'>User Info</h2>
+      <p className='fs-5 mt-3'>
+        Username: {user.username}<br/>
+        Email: {user.email}
+      </p>
+    </Col>
   );
 };
