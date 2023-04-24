f5013a988499997e0d16461d988beba3820a27a9
Stephen Weaver <sweaver1@ND.EDU>
2023-04-24 09:40:39 -0400

    Update movie-view.jsx
    Adjust spacing

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -9,7 +9,7 @@ import { MovieCard } from '../movie-card/movie-card';
 
 import './movie-view.scss'
 
-export const MovieView = ({ movies, user, updateUserOnFav }) => {
+export const MovieView = ({ movies, user, updateUser }) => {
   const { movieId } = useParams();
   const movie = movies.find((m) => m.id === movieId);
   window.scrollTo(0, 0);
@@ -24,59 +24,44 @@ export const MovieView = ({ movies, user, updateUserOnFav }) => {
   return (
     <>
       <Row className='d-flex p-3'>
-        <Col md={4} className='text-center text-md-end'>
+        <Col className='text-center text-md-end'>
           <img
             src={movie.imageURL}
             alt={`Poster for ${movie.title}`}
-            className='img-fluid h-100 w-auto movie-view-img'
+            className='img-fluid w-auto movie-view-img square bg-primary rounded-2 text-center text-md-end'
           />
         </Col>
-        <Col md={5} className='d-flex flex-column'>
-          <Row className='d-flex flex-row justify-content-between'>
-            <Col md={9} className='d-flex flex-column'>
-              <h2 className='my-0'>
-                <span>{movie.title}</span>
-              </h2>
-              <h5 className='mt-1 text-left text-muted fw-bolder'>
-                <span>{movie.year} | {movie.genre.name}</span>
-              </h5>
-            </Col>
-          </Row>
-          <div className='mt-2'>
+        <Col md={8} className='d-flex flex-column'>
+          <h2 className='my-0'>
+            <span>{movie.title}</span>
+          </h2>
+          <h5 className='mt-1 text-left text-muted fw-bolder'>
+            <span>{movie.year} | {movie.genre.name}</span>
+          </h5>
+          <p className='mt-2'>
             <span>{movie.description}</span>
-          </div>
-          <Row className='d-flex flex-row justify-content-between mt-auto mb-5 text-start'>
-            <FavoriteIcon
-              user={user}
-              movie={movie}
-              updateUserOnFav={updateUserOnFav}
-            />
-          </Row>
-          <Row className='mb-3'>
-            <Link to={`/`}>
-              <Button variant='secondary' size='md' className='fw-bold w-25'>
-                HOME
-              </Button>
-            </Link>
-          </Row>
-          <Row>
-            <Link to={`/users/${user.username}`}>
-              <Button variant='secondary' size='md' className='fw-bold w-25'>
-                PROFILE
-              </Button>
-            </Link>
-          </Row>
+          </p>
+          <FavoriteIcon
+            user={user}
+            movie={movie}
+            updateUser={updateUser}
+          />
+          <Link to={`/`}>
+            <Button variant='secondary' size='md' className='fw-bold w-auto mt-5'>
+              Back
+            </Button>
+          </Link>
         </Col>
       </Row>
       {similarMovies.length > 0 && (
         <Row>
           <h2 className='mt-5 mb-5 p-3'>SIMILAR MOVIES</h2>
           {similarMovies.map((movie) => (
-            <Col key={movie.id} xs={12} sm={6} md={4} lg={3}>
+            <Col key={movie.id} xl={2} lg={3} md={4} sm={12} xs={12}>
               <MovieCard
                 movie={movie}
                 user={user}
-                updateUserOnFav={(user) => {
+                updateUser={(user) => {
                   setUser(user);
                   localStorage.setItem('user', JSON.stringify(user));
                 }}
